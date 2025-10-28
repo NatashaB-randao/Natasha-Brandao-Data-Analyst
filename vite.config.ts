33ecfbb8a75b8e96b75eea4ea1146437841f94bf
NatashaB-randao <natashabrandao57@hotmail.com>
2025-10-28 14:56:29 -0300

    remover emoji

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Natasha-Brandao-Data-Analyst/', // 👈 importante para GitHub Pages
+  base: '/Natasha-Brandao-Data-Analyst/', // importante para GitHub Pages
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
     alias: {
